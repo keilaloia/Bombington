d8d412b4522f9c35fe5ada4e7305f75d9dd27db0
Joshua Livingston <s179132@students.aie.edu.au>
2017-12-13 14:34:06 -0800

    commit?

---
+++ Bomby/Assets/Scenes/MenuMain.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+
+public class MenuMain : MonoBehaviour {
+
+    public GameObject HostMenu;
+    public GameObject ClientMenu;
+
+    public Button HostButton;
+    public Button JoinButton;
+    public Button QuitButton;
+
+    public InputField HostPort;
+    public Button HostStart;
+    public Button HBack;
+
+    public InputField ClientAddress;
+    public InputField ClientPort;
+    public Button ClientStart;
+    public Button CBack;
+
+    public InputField PlayerName;
+    public Dropdown Teams;
+
+    public GameObject ServerAuthoritativePlayer;
+    public GameObject ClientAuthoritativePlayer;
+
+
+
+
+
+    void Start ()
+    {
+		
+	}
+	
+	void Update ()
+    {
+		
+	}
+}
---
+++ Bomby/Assets/TestingThangs.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestingThangs : MonoBehaviour {
+
+    public GameObject playaLob;
+
+	void Start ()
+    {
+		
+	}
+	
+    void spawnGuy()
+    {
+        if (Input.GetKey(KeyCode.P))
+        {
+            
+        }
+    }
+	void Update ()
+    {
+		
+	}
+}
